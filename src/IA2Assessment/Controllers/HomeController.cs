8801d27f0d39bc5d36cf9f7e22e2493c62faf20a
Voltstro <me@voltstro.dev>
2021-04-23 14:23:03 +1000

    Add initial project files

---
+++ src/IA2Assessment/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IA2Assessment.Controllers
+{
+	public class HomeController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
---
+++ src/IA2Assessment/Startup.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+
+namespace IA2Assessment
+{
+	public class Startup
+	{
+		private IConfiguration Configuration { get; set; }
+
+		// This method gets called by the runtime. Use this method to add services to the container.
+		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+		public void ConfigureServices(IServiceCollection services)
+		{
+			IConfigurationBuilder builder = new ConfigurationBuilder()   
+				.AddJsonFile("appsettings.json"); 
+			Configuration = builder.Build();
+			services.AddMvc(options => options.EnableEndpointRouting = false);
+		}
+
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
+
+			app.UseFileServer(); 
+			app.UseMvc(route =>
+			{
+				route.MapRoute("default", "{controller=Home}/{action=Index}/{id?}");
+			});
+		}
+	}
+}
